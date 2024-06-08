0829cdc99d4e19d395db5bdfa72f7feb9a7a381b
thomas <bezille.thomasb@gmail.com>
2024-06-08 16:50:05 +0200

    📦 NEW: Creation of an administration panel

---
+++ src/Controller/Admin/BookCrudController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Book;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+
+class BookCrudController extends AbstractCrudController
+{
+    use Trait\ReadOnlyTrait;
+
+    public static function getEntityFqcn(): string
+    {
+        return Book::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            IntegerField::new('pageCount'),
+            TextField::new('subtitle'),
+            TextField::new('description'),
+            TextField::new('isbn10'),
+            TextField::new('isbn13'),
+            ImageField::new('smallThumbnail'),
+            ImageField::new('thumbnail'),
+            CollectionField::new('authors'),
+            CollectionField::new('publishers'),
+            TextField::new('googleBooksId'),
+        ];
+    }
+    
+}
